fbdc3af5cc0426fa245426c2b19ca05807dd60f4
unknown <psamathe001@gmail.com>
2026-03-11 16:39:06 -0300

    app.js agregado para encender el servidor para levantar la pagina web trabajada anteriormente, README actualizado con las intruscciones

---
+++ version2/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const app = express()
+const PORT = 3000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname)));
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor encendido y corriendo en http://localhost:${PORT}`)
+})
